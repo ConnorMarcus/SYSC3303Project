7df3c1a1a0cf2bc8922ddb0c496017798bf575aa
Connor Marcus <73544123+ConnorMarcus@users.noreply.github.com>
2023-01-25 13:31:52 -0500

    Added initial code for ElevatorEvent class
    Added the initial code for the ElevatorEvent class which will hold data read from the text file.

--- src/com/sysc3303/project/ElevatorEvent.java
+++ src/com/sysc3303/project/ElevatorEvent.java
@@ -3,10 +3,65 @@
  */
 package com.sysc3303.project;
 
+import java.util.Date;
+
 /**
- * @author
+ * @author Connor Marcus
  *
  */
 public class ElevatorEvent {
-
+	public enum Direction {
+		UP,
+		Down;
+	}
+	
+	private final Date time;
+	private final int floorNumber;
+	private final Direction direction;
+	private final int carButtonNumber;
+	
+	/**
+	 * @param time the Date object corresponding to the time of the elevator request
+	 * @param floor the floor on which the elevator request was made
+	 * @param direction the direction of the elevator request
+	 * @param carButton the car button that was pressed in the elevator request
+	 */
+	public ElevatorEvent(Date time, int floor, Direction direction, int carButton) {
+		this.time = time;
+		this.floorNumber = floor;
+		this.direction = direction;
+		this.carButtonNumber = carButton;
+		
+		
+	}
+	
+	/**
+	 * @return the floor number on which the elevator request was made
+	 */
+	public int getFloorNumber() {
+		return floorNumber;
+	}
+	
+	/**
+	 * @return the direction of the elevator request
+	 */
+	public Direction getDirection() {
+		return direction;
+	}
+	
+	/**
+	 * @return the button number in the elevator that was pressed by the passenger
+	 */
+	public int getCarButton() {
+		return carButtonNumber;
+	}
+	
+	/**
+	 * @param otherEvent the other elevator event to compare this event to
+	 * @return true if this event occurs before the other event, and false otherwise
+	 */
+	public boolean eventOccursBefore(ElevatorEvent otherEvent) {
+		return this.time.before(otherEvent.time);
+	}
+	
 }
--- src/com/sysc3303/project/Floor.java
+++ src/com/sysc3303/project/Floor.java
@@ -3,6 +3,10 @@
  */
 package com.sysc3303.project;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author
  *
@@ -18,6 +22,18 @@ public class Floor implements Runnable {
 	public void run() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	/**
+	 * Helper method to create a Date object from a time string (called when reading event from text file)
+	 * 
+	 * @param time A time string in the following format hh:mm:ss.mmm
+	 * @return A Date object corresponding to the time string 
+	 * @throws ParseException An exception is thrown if the time string is not in the correct format
+	 */
+	public static Date createElevatorTime(String time) throws ParseException {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh:mm:ss.mmm");
+		return simpleDateFormat.parse(time);
 	}
 	
 	private void readEvent() {
